0b33fd4b761aa9b1fbd62e0c0b54ff334eaa015b
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-16 16:14:16 +0300

    updated jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- const [formData, setFormData] = React.useState({})
+ const [formData, setFormData] = React.useState(
+  {firstName: "", 
+   lastName: ""
+}
+  )
+
+  function handleChange(event) {
+    console.log(event.target.value)
+  }
 
   return (
     <div className="App">
@@ -11,9 +19,11 @@ function App() {
         <input 
             type="text" 
             placeholder='First Name'
+            onChange={handleChange}
         />
         <input 
             type="text" 
+            onChange={handleChange}
             placeholder='Last Name'
         />
       </form>
