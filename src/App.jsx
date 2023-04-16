b077e8b74a77ce8a13cfb6a55e3972551697c1b4
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-16 16:32:52 +0300

    updated jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import React from 'react'
 import './App.css'
 
 function App() {
- const [formData, setFormData] = React.useState(
-  {firstName: "", 
-   lastName: "",
-   email: ""
-}
+  const [formData, setFormData] = React.useState(
+    {
+      firstName: "",
+      lastName: "",
+      email: "",
+      comments: ""
+    }
   )
 
   function handleChange(event) {
@@ -17,34 +19,41 @@ function App() {
         ...prevFormData,
         [event.target.name]: event.target.value
       }
-    }) 
+    })
     console.log(formData)
   }
 
   return (
     <div className="App">
       <form action="" className='test-form'>
-        <input 
-            type="text" 
-            placeholder='First Name'
-            onChange={handleChange}
-            value={formData.firstName}
-            name="firstName"
+        <input
+          type="text"
+          placeholder='First Name'
+          onChange={handleChange}
+          value={formData.firstName}
+          name="firstName"
         />
-        <input 
-            type="text" 
-            onChange={handleChange}
-            placeholder='Last Name'
-            value={FormData.lastName}
-            name="lastName"
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder='Last Name'
+          value={FormData.lastName}
+          name="lastName"
         />
-        <input 
-            type="email" 
-            onChange={handleChange}
-            placeholder='Email'
-            value={FormData.email}
-            name="email"
+        <input
+          type="email"
+          onChange={handleChange}
+          placeholder='Email'
+          value={FormData.email}
+          name="email"
         />
+        <textarea
+          name="comments"
+          onChange={handleChange}
+          placeholder='Comments...'
+          value={FormData.comments}
+          cols="30"
+          rows="10"></textarea>
       </form>
     </div>
   )
